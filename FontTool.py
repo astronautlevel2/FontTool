af9dffb2fc16412b2b63b466e276deff2376522e
ihaveamac <ian@ianburgwin.net>
2016-04-25 07:29:34 -0700

    xor encryption which I wanted to finish before a release (closes #2)

--- FontTool.py
+++ FontTool.py
@@ -14,12 +14,14 @@ argParser = argparse.ArgumentParser()
 argParser.add_argument('-font', action='store', dest='fontFile', help='The font file location', required=True)
 argParser.add_argument('-output', action='store', dest='output',  help='Output location. Don\'t include file extension. Default is the name of the font file')
 argParser.add_argument('-force', action='store_true', dest='force', help='Overwrite existing files if they exist')
+argParser.add_argument('-xor', action='store_true', dest='xor', help='Encrypt NCCH if "0004009B00014002.Main.romfs.xorpad" exists')
 
 args = argParser.parse_args()
 
 fontFile = args.fontFile
 output = args.output
 force = args.force
+xor = args.xor
 
 def runCmd(cmd):
 	process = subprocess.Popen(cmd)
@@ -43,13 +45,24 @@ if not os.path.exists(args.fontFile):
 	print("Font file does not exist!")
 	sys.exit(1)
 
-raw_input("Your warranty ends here! Make backups of your NAND before you install this!")
+print("Your warranty ends here! Make backups of your NAND before you install this!")
 
-runCmd(["3dstool", "-zvf", "%s" % fontFile, "--compress-type", "lzex", "--compress-out", "cbf_std.bcfnt.lz"])
-os.mkdir("romfs")
-os.rename("cbf_std.bcfnt.lz", "romfs/cbf_std.bcfnt.lz")
+try:
+    os.makedirs("romfs")
+except OSError:
+    if not os.path.isdir("romfs"):
+        raise
+runCmd(["3dstool", "-zvf", "%s" % fontFile, "--compress-type", "lzex", "--compress-out", "romfs/cbf_std.bcfnt.lz"])
 runCmd(["3dstool", "-cvtf", "romfs", "romfs-mod.bin", "--romfs-dir", "romfs"])
-runCmd(["3dstool", "-cvtf", "cfa", "%s.cfa" % output, "--header", "ncchheader.bin", "--romfs", "romfs-mod.bin"])
+cmds = ["3dstool", "-cvtf", "cfa", "%s.cfa" % output, "--header", "ncchheader.bin", "--romfs", "romfs-mod.bin"]
+if xor:
+	print('xor romfs')
+	cmds.extend(["--romfs-xor", "0004009B00014002.Main.romfs.xorpad"])
+runCmd(cmds)
+if xor:
+	with open("%s.cfa" % output, "r+b") as fontcfa:
+		fontcfa.seek(0x18F)
+		fontcfa.write("\x00")
 if os.path.exists("%s.cia" % output) and not force:
 	uinput = raw_input("Warning! Output CIA already exists! Continue? (y/n): ")
 	if (uinput == "n"):
@@ -63,3 +76,5 @@ if os.path.exists("%s.cia" % output) and not force:
 
 runCmd(["make_cia", "-v", "-o", "%s.cia" % output, "--content0=%s.cfa" % output,  "--index_0=0"])
 clean()
+if not xor:
+	print("Encrypt NCCH using Decrypt9 before installing or it won't work!")
