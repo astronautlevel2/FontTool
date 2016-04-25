20ca1e1fdf420f4f69aacfd90a44ff59bf9c9348
alex <alex@ahsraiderreview.com>
2016-04-25 07:14:33 -0400

    Added ncchheader, made linux compatible

--- FontTool.py
+++ FontTool.py
@@ -10,15 +10,13 @@ import subprocess
 
 argParser = argparse.ArgumentParser()
 
-argParser.add_argument('-font', action='store', dest='fontFile', help='The font file location')
-argParser.add_argument('-output', action='store', dest='output', default="font-patch", help='Output location. Don\'t include file extension. Default is font-patch')
-argParser.add_argument('-fontCIA', action='store', dest='fontCIA', default="0004009B00014002.cia", help='Decrypted font CIA location. Default assumes 0004009B00014002.cia')
+argParser.add_argument('-font', action='store', dest='fontFile', help='The font file location', required=True)
+argParser.add_argument('-output', action='store', dest='output',  help='Output location. Don\'t include file extension. Default is the name of the font file')
 
 args = argParser.parse_args()
 
 fontFile = args.fontFile
 output = args.output
-fontCIA = args.fontCIA
 
 def runCmd(cmd):
 	process = subprocess.Popen(cmd)
@@ -26,24 +24,18 @@ def runCmd(cmd):
 	processoutput = process.communicate()[0]
 	if process.returncode != 0:
 		print("An error occured somewhere in the script. I was running: " + cmd)
+		sys.exit()
 
-if (fontFile is not None):
-	runCmd("3dstool -zvf " + fontFile + " --compress-type lzex --compress-out cbf_std.bcfnt.lz")
-	runCmd("ctrtool --contents=contents " + fontCIA)
-	runCmd("3dstool -xvtf cfa contents.0000.00000000 --header ncchheader.bin --romfs romfs.bin")
-	runCmd("3dstool -xvtf romfs romfs.bin --romfs-dir romfs")
-	os.remove("romfs/cbf_std.bcfnt.lz")
-	os.rename("cbf_std.bcfnt.lz", "romfs/cbf_std.bcfnt.lz")
-	runCmd("3dstool -cvtf romfs romfs-mod.bin --romfs-dir romfs")
-	runCmd("3dstool -cvtf cfa " + output + ".cfa --header ncchheader.bin --romfs romfs-mod.bin")
-	runCmd("makerom -f cia -content " + output + ".cfa:0")
-	os.remove("romfs/cbf_std.bcfnt.lz")
-	os.rmdir("romfs/")
-	os.remove("contents.0000.00000000")
-	os.remove(output + ".cfa")
-	os.remove("ncchheader.bin")
-	os.remove("romfs.bin")
-	os.remove("romfs-mod.bin")
-	
-else:
-	print("Make sure you have all the arguments!")
+if output is None:
+	output = os.path.splitext(fontFile)[0]
+
+runCmd(["3dstool", "-zvf", "%s" % fontFile, "--compress-type", "lzex", "--compress-out", "cbf_std.bcfnt.lz"])
+os.mkdir("romfs")
+os.rename("cbf_std.bcfnt.lz", "romfs/cbf_std.bcfnt.lz")
+runCmd(["3dstool", "-cvtf", "romfs", "romfs-mod.bin", "--romfs-dir", "romfs"])
+runCmd(["3dstool", "-cvtf", "cfa", "%s.cfa" % output, "--header", "ncchheader.bin", "--romfs", "romfs-mod.bin"])
+runCmd(["makerom", "-f", "cia", "-content", "%s.cfa:0" % output])
+os.remove("romfs/cbf_std.bcfnt.lz")
+os.rmdir("romfs/")
+os.remove(output + ".cfa")
+os.remove("romfs-mod.bin")
--- README.md
+++ README.md
@@ -3,11 +3,13 @@ This tool takes a font file with the format .bcfnt, which can be generated using
 
 It requires 3dstool, ctrtool, and makerom to function.
 
-In addition, it requires a decrypted CIA of the system font file.
-
 
 ## Installing
 Drop the generated CIA into the /D9Game directory of your SD card and use Decrypt9 to encrypt NCCH
+
+**Back up your NAND before installing this! If something goes wrong you'll be bricked**
+
+It's also kind of a pain to remove.
 
 It can then be installed using any CIA manager
 
@@ -18,4 +20,6 @@ See `FontTool.py -h`
 * `FontTool.py` is under the MIT license.
 * All other tools (makerom, ctrtool, and 3dstool) are under their respective licenses
 
-HUGE thanks to ihaveamac for making this possible
+HUGE thanks to ihaveamac for teaching me a lot of things and then bearing with my noob questions
+
+Also thanks to everyone on the 3dshacks discord for helping me test
